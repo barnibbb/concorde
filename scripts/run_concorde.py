bdd68eff114b53606e6390edc6cb939fcc24d4b0
barnibbb <barnibbb@gmail.com>
2025-05-05 16:13:02 +0200

    output file update

--- scripts/run_concorde.py
+++ scripts/run_concorde.py
@@ -1,18 +1,25 @@
 import sys
 import os
+import time
 
 from concorde.tsp import TSPSolver
 
 def run_concorde(tsp_file):
+    start = time.time()
+
     tsp_path = os.path.abspath(tsp_file)
     tsp_dir = os.path.dirname(tsp_path)
     tsp_name = os.path.splitext(os.path.basename(tsp_path))[0]
-    out_file = os.path.join(tsp_dir, tsp_name + "_pyconcorde.sol")
+    out_file = os.path.join(tsp_dir, tsp_name + ".sol")
 
     solver = TSPSolver.from_tspfile(tsp_path)
     solution = solver.solve()
 
+    end = time.time()
+    execution = end - start
+
     with open(out_file, 'w') as f:
+        f.write(f"{solution.optimal_value} {execution:.4f} ")
         f.write(" ".join(map(str, solution.tour)))
 
 
